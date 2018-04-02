271f748d00e74c791112d611064157bb577655c6
tdytaylor <949722633@qq.com>
2018-04-02 12:13:13 +0800

    初始化项目

---
+++ src/main/java/com/taylor/reputation/reputationspringmvc/ReputationSpringmvcApplication.java
@@ -0,0 +1,12 @@
+package com.taylor.reputation.reputationspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputationSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputationSpringmvcApplication.class, args);
+	}
+}
